f0b567c2b396d613970d50128e35df7601e071c2
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2025-08-25 11:13:24 +0300

    valid solution v.2

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -7,7 +7,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -28,7 +27,6 @@ public class Main {
 
             for (Future<String> future : futures) {
                 String string = future.get();
-//                System.out.println(string);
                 logger.info(string);
             }
         } catch (InterruptedException | ExecutionException e) {
